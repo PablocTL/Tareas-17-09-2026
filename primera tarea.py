2ea112fe6856555b35eb7dad9419b214d5e8099a
PablocTL <ptorrlan@myuax.com>
2026-09-17 19:40:25 +0200

    Correct insertion_sort implementation and add test case
    Fixed syntax errors in insertion_sort function and added test array.

--- primera tarea.py
+++ primera tarea.py
@@ -2,8 +2,11 @@ def insertion_sort(arr):
     for i in range(1,len(arr)):
         pos = arr[i]
         j = i-1
-        while j >= 0 and pos < arr[i]
+        while j >= 0 and pos < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = pos
     return arr
+
+arr = [1,5,4,7,9,10,17,3]
+print(insertion_sort(arr))
